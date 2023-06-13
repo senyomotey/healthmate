d775bd2103d6e0c969d3af1a945abafd9036f82a
Senyo Motey <moteysenyo@gmail.com>
2023-06-13 04:19:12 +0000

    New page rotutes added

--- routes/web.php
+++ routes/web.php
@@ -20,10 +20,14 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('visits', [VisitController::class, 'index'])->name('visits');
 Route::get('visit/add', [VisitController::class, 'create'])->name('visit/add');
+Route::get('visit/edit/{id}', [VisitController::class, 'edit'])->name('visit/edit');
 Route::post('visit/store', [VisitController::class, 'store'])->name('visit/store');
+Route::post('visit/update', [VisitController::class, 'update'])->name('visit/update');
 
 Route::get('patients', [PatientController::class, 'index'])->name('patients');
 Route::get('patient/add', [PatientController::class, 'create'])->name('patient/add');
+Route::get('patient/edit/{id}', [PatientController::class, 'edit'])->name('patient/edit');
 Route::post('patient/store', [PatientController::class, 'store'])->name('patient/store');
+Route::post('patient/update', [PatientController::class, 'update'])->name('patient/update');
 
 Route::get('reports', [HomeController::class, 'index'])->name('reports');
