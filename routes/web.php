55fee6aa4724421adc62f38a825fe50d3b3ab383
Senyo Motey <moteysenyo@gmail.com>
2023-06-13 00:22:07 +0000

    Added new page routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\VisitController;
+use App\Http\Controllers\PatientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+
+Route::get('visits', [VisitController::class, 'index'])->name('visits');
+Route::get('visit/add', [VisitController::class, 'create'])->name('visit/add');
+Route::post('visit/store', [VisitController::class, 'store'])->name('visit/store');
+
+Route::get('patients', [PatientController::class, 'index'])->name('patients');
+Route::get('patient/add', [PatientController::class, 'create'])->name('patient/add');
+Route::post('patient/store', [PatientController::class, 'store'])->name('patient/store');
+
+Route::get('reports', [HomeController::class, 'index'])->name('reports');
